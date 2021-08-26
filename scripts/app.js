01ee13051d9ac505cd68373ccc4dec0e61fb87e4
Leinier Perez <P_Leinier@yahoo.com>
2021-08-26 16:16:06 -0400

    Added gameHandler. Added init function to initialize the event listeners, logic for starting game, making a move, and checking who won.

--- scripts/app.js
+++ scripts/app.js
@@ -85,3 +85,113 @@ const displayController = (function displayController() {
     hideBoard,
   };
 })();
+
+const gameHandler = (function () {
+  let playerOne; // Player1 is always X
+  let playerTwo; // Player2 is always O
+  let currentPlayer;
+
+  const form = document.querySelector('form');
+  const boardCells = document.querySelector('.board-cells');
+  const player1Input = document.querySelector('#player1-name');
+  const player2Input = document.querySelector('#player2-name');
+  const submitBtn = document.querySelector('.submit-btn');
+  const startBtn = document.querySelector('.start-btn');
+  const cells = document.querySelectorAll('.cell');
+  const restartBtn = document.querySelector('#restart-btn');
+  const newGameBtn = document.querySelector('#newgame-btn');
+
+  const init = () => {
+    submitBtn.addEventListener('click', startGame);
+    restartBtn.addEventListener('click', restartGame);
+    newGameBtn.addEventListener('click', newGame);
+    startBtn.addEventListener(
+      'click',
+      displayController.showPlayerNameSelectionScreen
+    );
+  };
+
+  const startGame = (event) => {
+    event.preventDefault();
+    if (player1Input.value && player2Input.value) {
+      form.style.cssText = 'display:none';
+      boardCells.style.cssText = 'display:grid';
+    }
+    playerOne = player(player1Input.value, 'X');
+    playerTwo = player(player2Input.value, 'O');
+    makeMove();
+  };
+
+  const makeMove = () => {
+    const board = gameBoard.getBoard();
+    currentPlayer = 'X';
+
+    cells.forEach((c) => {
+      c.addEventListener('click', (event) => {
+        if (event.target.querySelector('span') === null) return;
+
+        const span = event.target.querySelector('span');
+        const dataKey = span.dataset.key;
+
+        if (!span.innerHTML) {
+          if (
+            board[dataKey[0]][dataKey[2]] === 'X' ||
+            board[dataKey[0]][dataKey[2]] === 'O'
+          ) {
+            currentPlayer = board[dataKey[0]][dataKey[2]];
+          }
+          board[dataKey[0]][dataKey[2]] = currentPlayer;
+          span.innerHTML = currentPlayer;
+          currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+          const winner = checkWinner(playerOne.getPlayerSymbol())
+            ? playerOne.getPlayerName()
+            : checkWinner(playerTwo.getPlayerSymbol())
+            ? playerTwo.getPlayerName()
+            : null;
+          displayController.displayWinner(winner);
+        }
+      });
+    });
+  };
+
+  const restartGame = () => {
+    gameBoard.resetBoard();
+    currentPlayer = 'X';
+    displayController.hideWinnerMessageScreen();
+    displayController.render();
+  };
+
+  const newGame = () => {
+    gameBoard.resetBoard();
+    displayController.hideWinnerMessageScreen();
+    displayController.showPlayerNameSelectionScreen();
+    displayController.hideBoard();
+    displayController.render();
+  };
+
+  const checkWinner = (currentPlayer) => {
+    const board = gameBoard.getBoard();
+
+    const winningCombos = [
+      [[0,0], [0,1], [0,2]],
+      [[1,0], [1,1], [1,2]],
+      [[2,0], [2,1], [2,2]],
+      [[0,0], [1,0], [2,0]],
+      [[0,1], [1,1], [2,1]],
+      [[0,2], [1,2], [2,2]],
+      [[0,0], [1,1], [2,2]],
+      [[0,2], [1,1], [2,0]],
+    ];
+
+    return winningCombos.some((combination) => {
+      return combination.every(([x, y]) => {
+        return board[x][y] === currentPlayer;
+      });
+    });
+  };
+
+  return { init };
+})();
+
+gameHandler.init();
